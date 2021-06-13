53fe85518a7ee050f801aa980060c271d0b32613
Scaffolder <scaffolder@backstage.io>
2021-06-12 19:37:21 -0700

    Initial commit

---
+++ .ngif/setup-repo.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+echo "Determining if the template has been initialized..."
+
+git_branches=$(git branch)
+
+if ! [[ "${git_branches}" =~ "template" ]]; then
+    git checkout template
+    git checkout master
+
+    echo "Template needs to be initialized, initializing..."
+    ./.ngif/initialize-template.sh
+else
+    echo "Template has been previously initialized."
+fi
